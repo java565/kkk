3246b0fa942d94fa3f29e1a1c85c7128a053c48b
java565 <74774836+java565@users.noreply.github.com>
2020-11-24 21:31:26 +0800

    Create 112402App.java

---
+++ 112402App.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class kk {
+    JPanel myPanel = new JPanel();
+    JLabel label_txt = new JLabel();
+    JTextArea textArea_A = new JTextArea();
+    JTextArea textArea_B = new JTextArea();
+
+//    构造方法;
+    public kk () {
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (textArea_A.getText().equals("kmj")) {
+                        ;
+                        textArea_B.setText("输入正确");
+                    } else {
+                        textArea_B.setText("输入错误");
+                    }
+                }
+            }
+        });
+    }
+
+    //显示常规方法
+    void go() {
+        textArea_A.setBounds(100, 100, 300, 100);
+        textArea_A.setBackground(Color.blue);
+        textArea_B.setBounds(100, 200, 300, 100);
+        textArea_B.setBackground(Color.magenta);
+        myPanel.add(textArea_A);
+        myPanel.add(textArea_B);
+        JFrame frame = new JFrame("123456789");
+        myPanel.setLayout(null);
+        myPanel.add(label_txt);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 512, 720);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new kk().go();
+    }
+}
+
