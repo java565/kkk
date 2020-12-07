0961bda891faf18ada6bf764f7cab6c1e28bcac8
java565 <74774836+java565@users.noreply.github.com>
2020-12-07 15:31:45 +0800

    Create 120701.主类.App.java

---
+++ 120701.主类.App.java
@@ -0,0 +1,17 @@
+public class App {
+	public static void main(String[] args) {
+		int array2d[][]={
+			{1,2,3,4,5,6},
+			{7,8,9,10,0,12},
+			{8,8,9,10,11,12},
+			{7,8,3,10,11,12},
+			{88,66,92,102,151,999}
+		};
+
+		new Find().findZero(array2d);
+		new Find().R_left(array2d);
+		new Find().R_right(array2d);
+		new Find().R_up(array2d);
+		new Find().R_down(array2d);
+	}
+}
