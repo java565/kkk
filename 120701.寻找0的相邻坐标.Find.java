32b1faf0d77115cac3897ffe40cffafe92809caa
java565 <74774836+java565@users.noreply.github.com>
2020-12-07 15:53:42 +0800

    Update 120701.寻找0的相邻坐标.Find.java

--- 120701.寻找0的相邻坐标.Find.java
+++ 120701.寻找0的相邻坐标.Find.java
@@ -1,16 +1,6 @@
 
 public class Find {
-void findZero(int array2d[][]){
-			for (int i = 0; i < array2d.length; i++) {
-				for (int j = 0; j < array2d[i].length; j++) {
-					if (array2d[i][j] == 0) {
-						System.out.println("R_Zero("+i+","+j+")");
-						break;
-					}
-				}
-			}
-		}
-		
+
 		void R_left(int array2d[][]){
 			for (int i = 0; i < array2d.length; i++) {
 				for (int j = 0; j < array2d[i].length; j++) {
@@ -21,7 +11,26 @@ void findZero(int array2d[][]){
 				}
 			}
 		}
-		
+		void findZero(int array2d[][]){
+			for (int i = 0; i < array2d.length; i++) {
+				for (int j = 0; j < array2d[i].length; j++) {
+					if (array2d[i][j] == 0) {
+						System.out.println("R_Zero("+i+","+j+")");
+						break;
+					}
+				}
+			}
+		}
+		void R_down(int array2d[][]){
+			for (int i = 0; i < array2d.length; i++) {
+				for (int j = 0; j < array2d[i].length; j++) {
+					if (array2d[i][j] == 0) {
+						System.out.println("R_left("+j+","+(i+1)+")");
+						break;
+					}
+				}
+			}
+		}
 		void R_right(int array2d[][]){
 			for (int i = 0; i < array2d.length; i++) {
 				for (int j = 0; j < array2d[i].length; j++) {
@@ -37,21 +46,11 @@ void findZero(int array2d[][]){
 			for (int i = 0; i < array2d.length; i++) {
 				for (int j = 0; j < array2d[i].length; j++) {
 					if (array2d[i][j] == 0) {
-						System.out.println("R_left("+(i-1)+","+j+")");
+						System.out.println("R_up("+j+","+(i-1)+")");
 						break;
 					}
 				}
 			}
 		}
 		
-		void R_down(int array2d[][]){
-			for (int i = 0; i < array2d.length; i++) {
-				for (int j = 0; j < array2d[i].length; j++) {
-					if (array2d[i][j] == 0) {
-						System.out.println("R_left("+(i+1)+","+j+")");
-						break;
-					}
-				}
-			}
-		}
-	}
+}
