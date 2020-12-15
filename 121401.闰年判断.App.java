df21e32a56cb7c315c7c8f4fabe710933d62e837
java565 <74774836+java565@users.noreply.github.com>
2020-12-15 11:31:37 +0800

    Create 121401.闰年判断.App.java

---
+++ 121401.闰年判断.App.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class App1 {
+    public static void main(String[] args) {
+        long sum = 0;
+        for (long i = 0;i<=2147483647;i++){
+            if (i%2==1){
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
